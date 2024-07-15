0155b68a430b4cdef6055ee4c7bbcb939df0b6ef
Abdullah <abdullahaviator13@gmail.com>
2024-07-15 01:23:09 -0400

    f

--- app.py
+++ app.py
@@ -173,6 +173,8 @@ def open_crates():
         chrome_options.add_argument('--no-sandbox')
         chrome_options.add_argument('--disable-dev-shm-usage')
         chrome_options.add_argument('--headless')
+        chrome_options.add_argument('--disable-gpu')
+
 
         
         # Initialize undetected Chrome WebDriver with options
